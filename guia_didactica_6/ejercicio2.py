4008832c8ef82f20b038306717075c2124993a8d
jo1331xd <josecarvallo777@gmail.com>
2025-05-01 21:30:17 -0600

    ejercicio 2 guia

---
+++ guia_didactica_6/ejercicio2.py
@@ -0,0 +1,6 @@
+def mostrar_pares ():
+    numero=int(input("Ingrese un numero positivo "))
+    for i in range (0, numero, + 1):
+        if i %2 == 0:
+            print(i)
+mostrar_pares()         
